dda668b16270b3e067b13bf88dd9366d6191eb56
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-16 23:59:00 +0700

    fix logo path

--- src/js/ini-header.js
+++ src/js/ini-header.js
@@ -6,8 +6,8 @@ class IniHeader extends HTMLElement {
 
     render() {
         this.innerHTML = `
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a class="navbar-brand font-weight-bold" href="https://irecipe.innaweb.com/dist/"><span class="font-weight-lighter">i</span>Recipe</a><span class="collapse navbar-collapse slogan text-white font-italic font-weight-lighter">"Remember Recipes, Remember iRecipe!"</span>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark shadow">
+            <a class="navbar-brand font-weight-bold" href="${window.location.origin}"><span class="font-weight-lighter">i</span>Recipe</a><span class="collapse navbar-collapse slogan text-white font-italic font-weight-lighter">"Remember Recipes, Remember iRecipe!"</span>
             <div class="" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
                 </ul>
