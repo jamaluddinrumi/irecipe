05dc6ce2d57f989b6067435899167c06fe0124ef
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-16 16:00:07 +0700

    turn search button into input-group & create foote

--- src/js/kolom-pencarian.js
+++ src/js/kolom-pencarian.js
@@ -15,10 +15,12 @@ class KolomPencarian extends HTMLElement {
 
     render() {
         this.innerHTML = `
-        <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="e.g. beef, chicken, etc" aria-label="Search">
-            <button id="cari-resep" class="btn btn-light my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
-        </form>
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="e.g. beef, chicken, etc" aria-label="e.g. beef, chicken, etc" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button id="cari-resep" class="btn btn-secondary" type="button"><i class="fa fa-search"></i></button>
+            </div>
+        </div>
         `;
 
         this.querySelector('#cari-resep').addEventListener('click', this._clickEvent);
