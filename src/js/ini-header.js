6f72938e704ef80f362013427aa7d1ba1bfa3039
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-18 22:21:19 +0700

    fix navbar according bootstrap 5

--- src/js/ini-header.js
+++ src/js/ini-header.js
@@ -7,10 +7,12 @@ class IniHeader extends HTMLElement {
     render() {
         this.innerHTML = `
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark shadow">
-            <a class="navbar-brand font-weight-bold" href="${window.location.href}"><span class="font-weight-lighter">i</span>Recipe</a><span class="collapse navbar-collapse slogan text-white font-italic font-weight-lighter">"Remember Recipes, Remember iRecipe!"</span>
-            <div class="" id="navbarCollapse">
-                <ul class="navbar-nav mr-auto">
-                </ul>
+            <div class="container-fluid">
+                <a class="navbar-brand font-weight-bold" href="${window.location.href}"><span class="font-weight-lighter">i</span>Recipe</a><span class="collapse navbar-collapse slogan text-white font-italic font-weight-lighter">"Remember Recipes, Remember iRecipe!"</span>
+                <div class="" id="navbarCollapse">
+                    <ul class="navbar-nav mr-auto">
+                    </ul>
+                </div>
             </div>
         </nav>
         `;
