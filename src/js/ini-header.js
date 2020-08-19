f70bbdd918e8fb1ed1a9f063e6e9e35d62ff1888
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-20 00:36:42 +0700

    fix not found background image

--- src/js/ini-header.js
+++ src/js/ini-header.js
@@ -8,9 +8,14 @@ class IniHeader extends HTMLElement {
         this.innerHTML = `
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark shadow">
             <div class="container-fluid">
-                <a class="navbar-brand font-weight-bold" href="${window.location.href}"><span class="font-weight-lighter">i</span>Recipe</a><span class="collapse navbar-collapse slogan text-white font-italic font-weight-lighter">"Remember Recipes, Remember iRecipe!"</span>
-                <div class="" id="navbarCollapse">
-                    <ul class="navbar-nav mr-auto">
+                <a class="navbar-brand font-weight-bold" href="${window.location.href}">
+                    <span class="font-weight-lighter">i</span>Recipe
+                </a>
+                <span class="collapse navbar-collapse slogan text-white font-italic font-weight-lighter">
+                    "Remember Recipes, Remember iRecipe!"
+                </span>
+                <div class="" id="navbar-search">
+                    <ul class="nav navbar-nav">
                     </ul>
                 </div>
             </div>
@@ -21,12 +26,12 @@ class IniHeader extends HTMLElement {
     set kolomPencarian(kolomPencarianElement) {
         this._kolomPencarianElement = kolomPencarianElement;
 
-        const navbar = this.querySelector('#navbarCollapse');
+        const navbar = this.querySelector('#navbar-search');
         navbar.appendChild(this._kolomPencarianElement);
     }
 
     get kolomPencarian() {
-        const navbar = this.querySelector('#navbarCollapse');
+        const navbar = this.querySelector('#navbar-search');
         navbar.removeChild(this._kolomPencarianElement);
         
         return this._kolomPencarianElement;
