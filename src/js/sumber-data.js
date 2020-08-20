be20be64a8ae618e97a13b2170dcae1d192f0f6b
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-21 02:55:17 +0700

    fix stale-while-revalidate

--- src/js/sumber-data.js
+++ src/js/sumber-data.js
@@ -2,7 +2,7 @@ class SumberData {
     static cariResep(keyword) {
         return fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
                 .then(response => {
-                    response.setHeader("Cache-Control: max-age=1, stale-while-revalidate=59");
+                    response.setHeader("Cache-Control",  "s-maxage=1", "stale-while-revalidate=59");
                     return response.json();
                 })
                 .then(responseJson => {
