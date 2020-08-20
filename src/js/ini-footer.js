ea9a88eaaa9c08c950c782c4c09aa1150de0a640
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-20 13:57:52 +0700

    change email address

--- src/js/ini-footer.js
+++ src/js/ini-footer.js
@@ -15,7 +15,7 @@ class IniFooter extends HTMLElement {
             <path fill-rule="evenodd" d="M12 4H4v8a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4zM3 3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V3H3z"/>
             <path fill-rule="evenodd" d="M14.5 5.5h-2v-1h2A1.5 1.5 0 0 1 16 6v4a1.5 1.5 0 0 1-1.5 1.5h-2v-1h2a.5.5 0 0 0 .5-.5V6a.5.5 0 0 0-.5-.5z"/>
         </svg>
-        by <a href="mailto:udin@algostudio.net">Jamaluddin Rumi</a></p>
+        by <a href="mailto:udin@algostudio.net">udin@algostudio.net</a></p>
         `;
     };
 }
