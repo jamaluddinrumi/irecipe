071e9f9f07391127be40945de5e73e17c78ca139
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-23 00:41:54 +0700

    add chunk plugin more more

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,26 +2,37 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: "/node_modules/",
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"]
-                        }
-                    }
-                ]
-            }
-        ]
-    }
-})
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
