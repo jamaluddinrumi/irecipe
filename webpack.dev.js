0fbe42aac7d73c984ae706e53bbb7a0381de0d78
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-23 00:15:36 +0700

    using chunks

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,10 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+  
 
-module.exports = merge( common, {
-    mode: "development"
+module.exports = merge(common, {
+  mode: "development",
+  plugins: [new BundleAnalyzerPlugin()]
 });
