8861905b42cc660898c5190006bee0cc47d12504
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-22 16:28:15 +0700

    add meta x-ua-compatible to head html

--- src/js/html.js
+++ src/js/html.js
@@ -3,6 +3,8 @@ mainElement.lang = 'en';
 
 const title = document.querySelector('title');
 title.insertAdjacentHTML('afterend', `
+<meta http-equiv="x-ua-compatible" content="ie=edge"/>
+
 <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
